bc2d0351ea5bf0d5d21fa896f3ebc91367feb057
8GutiMu <you@example.com>
2017-06-05 17:04:03 -0500

    ultimos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var cargarPagina = function () {
 	obtenerCoordendas();
      mostrarRestaurantes();
-    $("#buscador").click(filtrarContactos);
+    $("#buscador").submit(filtrarContactos);
 };
 
 var obtenerCoordendas = function () {
@@ -23,9 +23,40 @@ var mostrarMapa = function (coordenadas) {
     });
 }
 
-var mostrarRestaurantes = function () {
+var c = function () {
 
-    var arregloRestaurantes = [
+   
+    var plantillaContacto =
+        '<article class="restaurante00">' +
+        '<div class="col s12 m7">' +
+        '<div class="card horizontal">' +
+        '<div class="card-image">' +
+        '<img src="https://dummyimage.com/100x110/000/fff">' +
+        '</div>' +
+        ' <div class="card-stacked">' +
+        '<div class="card-content">' +
+        '<h6><strong>__nombre__</strong></h6>' +
+        '<p>__direccion__</p>' +
+        '<p class="tamanio-letra-chico">Tipo: __tipoDeComida__</p>' +
+        ' </div>' +
+        ' </div>' +
+        ' </div>' +
+        '</div>' +
+        '</article>';
+
+    var plantillaFinal = "";
+
+    arregloRestaurantes.forEach(function (contacto) {
+        plantillaFinal += plantillaContacto.replace("__nombre__", contacto.nombre)
+            .replace("__direccion__", contacto.direccion)
+            .replace("__tipoDeComida__", contacto.comida);
+    });
+    $("#sectionRes").html(plantillaFinal);
+
+
+}
+
+ var arregloRestaurantes = [
         {
             nombre: "Zapote",
             direccion: "Calle Guanajuato 138, Cuauhtémoc, Roma Nte., 06700 Ciudad de México, CDMX",
@@ -63,41 +94,13 @@ var mostrarRestaurantes = function () {
     }
 ];
 
-    var plantillaContacto =
-        '<article class="restaurante00">' +
-        '<div class="col s12 m7">' +
-        '<div class="card horizontal">' +
-        '<div class="card-image">' +
-        '<img src="https://dummyimage.com/100x110/000/fff">' +
-        '</div>' +
-        ' <div class="card-stacked">' +
-        '<div class="card-content">' +
-        '<h6><strong>__nombre__</strong></h6>' +
-        '<p>__direccion__</p>' +
-        '<p class="tamanio-letra-chico">Tipo: __tipoDeComida__</p>' +
-        ' </div>' +
-        ' </div>' +
-        ' </div>' +
-        '</div>' +
-        '</article>';
-
-    var plantillaFinal = "";
-
-    arregloRestaurantes.forEach(function (contacto) {
-        plantillaFinal += plantillaContacto.replace("__nombre__", contacto.nombre)
-            .replace("__direccion__", contacto.direccion)
-            .replace("__tipoDeComida__", contacto.comida);
-    });
-    $("#sectionRes").html(plantillaFinal);
-
-
-}
 
 var filtrarContactos = function (e) {
 	e.preventDefault();
 	var criterioBusqueda = $("#search").val().toLowerCase();
-	var contactosFiltrados = contactos.filter(function (contacto) {
-		return contacto.nombre.toLowerCase().indexOf(criterioBusqueda) >= 0;
+	var contactosFiltrados = arregloRestaurantes.filter(function (contacto) {
+		var nuevos = contacto.nombre.toLowerCase().indexOf(criterioBusqueda) >= 0;
+        return nuevos;
 	});
 	mostrarRestaurantes(contactosFiltrados);
 };
